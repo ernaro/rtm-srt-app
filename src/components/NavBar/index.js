ba6be9aeecf5e79f04a14670c7524718322b1765
ernaro <askopych@ukr.net>
2022-08-22 09:26:49 +0300

    update

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -9,7 +9,7 @@ const NavBar = () => {
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          ReTransmit
+          SRT ReTransmit
         </Typography>
         <IconButton>
           <AccountCircle />
